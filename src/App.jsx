50cc7beacfab58bf230b0c4aa652f6eb55d65523
marthons <tonmarchetti.im@gmail.com>
2024-06-21 18:55:11 -0300

    Finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,117 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from 'styled-components';
+import EstilosGlobais from './componente/EstilosGlobais';
+import Cabecalho from './componente/Cabecalho';
+import BarraLateral from './componente/BarraLateral';
+import bannerBackground from './assets/banner.png'
+import Banner from './componente/Banner';
+import Galeria from './componente/Galeria';
+import fotos from './fotos.json'
+import { useEffect, useState } from 'react';
+import ModalZoom from './componente/ModalZoom';
+import Rodape from './componente/RodaPe';
 
-function App() {
-  const [count, setCount] = useState(0)
+const FundoGradiente = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  min-height: 100vh;
+`;
+
+const AppContainer = styled.div`
+  width: 1330px;
+  margin: 0 auto;
+  max-width: 100%;
+`;
+
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`
+
+const ConteudoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
+
+const App = () => {
+
+  const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
+  const [fotoSelecionada, setFotoSelecionada] = useState(null)
+  const [filtro, setFiltro] = useState('')
+  const [tag, setTag] = useState(0)
+
+  useEffect(() => {
+    const fotosFiltradas = fotos.filter(foto => {
+      const filtroPorTag = !tag || foto.tagId === tag;
+      const filtroPorTitulo = !filtro || foto.titulo.toLowerCase().includes(filtro.toLowerCase())
+      return filtroPorTag && filtroPorTitulo
+    })
+    setFotosDaGaleria(fotosFiltradas)
+  }, [filtro, tag])
+
+  const aoAlternarFavorito = (foto) => {
+    if (foto.id === fotoSelecionada?.id) {
+      setFotoSelecionada({
+        ...fotoSelecionada,
+        favorita: !fotoSelecionada.favorita
+      })
+    }
+    setFotosDaGaleria(fotosDaGaleria.map(fotoDaGaleria => {
+      return {
+        ...fotoDaGaleria,
+        favorita: fotoDaGaleria.id === foto.id ? !foto.favorita : fotoDaGaleria.favorita
+      }
+    }))
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <FundoGradiente>
+
+      <EstilosGlobais />
+
+      <AppContainer>
+
+        <Cabecalho
+          filtro={filtro}
+          setFiltro={setFiltro}
+         />
+
+        <MainContainer>
+
+          <BarraLateral />
+
+          <ConteudoGaleria>
+
+            <Banner
+              texto="A galeria mais completa de fotos do espaço!"
+              backgroundImage={bannerBackground}
+            />
+
+            <Galeria
+              aoFotoSelecionada={foto => setFotoSelecionada(foto)}
+              aoAlternarFavorito={aoAlternarFavorito}
+              fotos={fotosDaGaleria}
+              setTag={setTag}
+            />
+
+          </ConteudoGaleria>
+
+
+
+        </MainContainer>
+
+      </AppContainer>
+
+      <ModalZoom
+        foto={fotoSelecionada}
+        aoFechar={() => setFotoSelecionada(null)}
+        aoAlternarFavorito={aoAlternarFavorito}
+      />
+
+      <Rodape />
+
+    </FundoGradiente>
+
   )
 }
 
